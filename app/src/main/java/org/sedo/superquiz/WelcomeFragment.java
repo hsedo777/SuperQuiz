9e7f7fd342f7e79ca90706964f23bf762afa7bd3
hsedo777 <hsedo777@gmail.com>
2023-10-03 01:17:13 +0100

    Updates WelcomeFragment.java to use view binding functionality instead of layout

--- app/src/main/java/org/sedo/superquiz/WelcomeFragment.java
+++ app/src/main/java/org/sedo/superquiz/WelcomeFragment.java
@@ -2,11 +2,15 @@ package org.sedo.superquiz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.sedo.superquiz.databinding.FragmentWelcomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +23,8 @@ public class WelcomeFragment extends Fragment {
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 	private static final String ARG_PARAM1 = "param1";
 	private static final String ARG_PARAM2 = "param2";
+
+	private FragmentWelcomeBinding binding;
 
 	// TODO: Rename and change types of parameters
 	private String mParam1;
@@ -59,6 +65,14 @@ public class WelcomeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_welcome, container, false);
+		binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+		return binding.getRoot();
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		//Deactivate the button until the text field become not empty
+		binding.playGame.setEnabled(false);
 	}
 }
